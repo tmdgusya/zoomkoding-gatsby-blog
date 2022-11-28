1ba44c32444c088ecb5eb3811a24a769798655ad
tmdgusya <tmdgusya@naver.com>
2022-11-28 13:37:41 +0900

    change default asset

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `Rlog`,
+  description: `Roach Dev log`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://www.roach-dev.com/`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
@@ -11,16 +11,16 @@ module.exports = {
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `정승현`,
     bio: {
-      role: `개발자`,
-      description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
-      thumbnail: 'sample.png', // Path to the image in the 'asset' folder
+      role: `엔지니어`,
+      description: ['기술로 편리한 세상을 만드는'],
+      thumbnail: 'profile.jpeg', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/tmdgusya`, // `https://github.com/zoomKoding`,
+      linkedIn: `https://www.linkedin.com/in/%EC%8A%B9%ED%98%84-%EC%A0%95-376842221/`, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
+      email: `dev0jsh@gmail.com`, // `zoomkoding@gmail.com`,
     },
   },
 
@@ -30,8 +30,8 @@ module.exports = {
       // =====       [Timestamp Sample and Structure]      =====
       // ===== 🚫 Don't erase this sample (여기 지우지 마세요!) =====
       {
-        date: '',
-        activity: '',
+        date: '2021.11 ~ 재직중',
+        activity: '우아한 형제들 서버개발자',
         links: {
           github: '',
           post: '',
@@ -43,12 +43,17 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        date: '2021.11 ~ 재직중',
+        activity: '우아한 형제들 서버개발자',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post: 'https://www.linkedin.com/in/%EC%8A%B9%ED%98%84-%EC%A0%95-376842221/',
+        },
+      },
+      {
+        date: '2021.05 ~ 2021.11',
+        activity: '청소연구소 백엔드 서버 개발자',
+        links: {
+          post: 'https://www.linkedin.com/in/%EC%8A%B9%ED%98%84-%EC%A0%95-376842221/',
         },
       },
     ],
@@ -72,15 +77,13 @@ module.exports = {
       // ========================================================
       // ========================================================
       {
-        title: '개발 블로그 테마 개발',
+        title: '코틀린 테스트 코드 생성 자동화 프로그램',
         description:
-          '개발 블로그를 운영하는 기간이 조금씩 늘어나고 점점 많은 생각과 경험이 블로그에 쌓아가면서 제 이야기를 담고 있는 블로그를 직접 만들어보고 싶게 되었습니다. 그동안 여러 개발 블로그를 보면서 좋았던 부분과 불편했던 부분들을 바탕으로 레퍼런스를 참고하여 직접 블로그 테마를 만들게 되었습니다.',
-        techStack: ['gatsby', 'react'],
-        thumbnailUrl: 'blog.png',
+          '유닛 테스트 코드에서 항상 작성해야 하는 Mock 객체 등등을 자동으로 파일을 분석하여 만들어주는 프로그램입니다.',
+        techStack: ['kotlin', 'intellij engine'],
+        thumbnailUrl: 'kotest.jpeg',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          github: 'https://github.com/tmdgusya/kotlin-test-boilerplate',
         },
       },
     ],
